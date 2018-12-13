9b3f8a711195ba573f7c6d0829adfae2123baeb9
Tumbleweeeed <916539494@qq.com>
2018-12-13 14:26:13 +0800

    轮播图

--- slideShow/main.js
+++ slideShow/main.js
@@ -1,8 +1,30 @@
 var slide = document.querySelector('.box-slide');
 var left = document.querySelector('.box-left');
 var right = document.querySelector('.box-right');
-var i = 0;
+var font = document.querySelector('.box-dot');
+var dot = font.querySelectorAll('span');
+var img = slide.querySelectorAll('img');
+var i = 0,
+	k = 45;
 right.onclick = function(){
 	i++;
-	slide.style.transform = "rotateY(" + i +"deg)";
+	slide.style.transform = "rotateY(" + i*k +"deg)";
+}
+
+left.onclick = function(){
+	i--;
+	slide.style.transform = "rotateY("+ i*k +"deg)";
+}
+
+console.log(dot);
+console.log(img[1]);
+
+
+for(i=0;i<img.length;i++){
+	(function(i){
+			dot[i].onclick = function(){
+				slide.style.transform = "rotateY("+ i*k +"deg)";
+				console.log(i);
+			}
+		})(i);
 }
