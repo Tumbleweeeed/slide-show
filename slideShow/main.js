234081c627e232b151c1077ff84873ed77e443aa
Tumbleweeeed <916539494@qq.com>
2018-12-17 16:20:24 +0800

    js

--- slideShow/main.js
+++ slideShow/main.js
@@ -4,27 +4,59 @@ var right = document.querySelector('.box-right');
 var font = document.querySelector('.box-dot');
 var dot = font.querySelectorAll('span');
 var img = slide.querySelectorAll('img');
-var i = 0,
+var j = 0,
+	n = 0,
 	k = 45;
+
+function removeClass(){
+	for(x=0;x<img.length;x++){
+		dot[x].classList.remove('active');
+	}
+}
+
 right.onclick = function(){
-	i++;
-	slide.style.transform = "rotateY(" + i*k +"deg)";
+	j++;
+	if(j>7){
+		j=0;
+	}
+	removeClass();
+	dot[j].classList.add('active');
+	n++;
+	slide.style.transform = "rotateY(" + n*k +"deg)";
 }
 
 left.onclick = function(){
-	i--;
-	slide.style.transform = "rotateY("+ i*k +"deg)";
+	j--;
+	if(j<0){
+		j=7;
+	}
+	console.log(j);
+	removeClass();
+	dot[j].classList.add('active');
+	n--;
+	slide.style.transform = "rotateY(" + n*k +"deg)";
+
 }
 
 console.log(dot);
 console.log(img[1]);
 
-
-for(i=0;i<img.length;i++){
+for(var i=0;i<img.length;i++){
 	(function(i){
 			dot[i].onclick = function(){
 				slide.style.transform = "rotateY("+ i*k +"deg)";
-				console.log(i);
+				removeClass();
+				dot[i].classList.add('active');
+				j=i;
 			}
 		})(i);
 }
+
+// 待完成
+// for(i=0;i<img.length;i++){
+// 	imgDeg = img[i].style.transform;
+// 	if(imgDeg == "rotateY(45deg)"){
+// 		removeClass();
+// 		dot[i].classList.add('active');
+// 	}
+// }
